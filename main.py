bd344a6043b8ca491e0ff2b2099bc35f29545401
levchenkopavlo <levchenkopavlo@gmail.com>
2024-03-12 19:58:22 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+class Animals:
+    def move(self):
+        print('Рухається')
+
+    def eat_food(self):
+        print('їсть')
+
+
+class Dogs(Animals):
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+
+
+class Cats(Animals):
+    pass
+
+
+# створення екземпляру класу
+my_dog = Dogs("Rex", 2, "bulldog")
+# методи
+# my_dog.eat_food()
+# my_dog.move()
+print(my_dog.name)
