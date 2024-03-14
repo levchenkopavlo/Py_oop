4e6ee87c073b738b55702facfe70239d14886dbf
levchenkopavlo <levchenkopavlo@gmail.com>
2024-03-14 20:10:52 +0200

    lab2 task1

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-class Animals:
-    def move(self):
-        print('Рухається')
+# Завдання 1
+#  Створіть клас «Дріб». Збережіть у класі чисельник
+# та знаменник. Реалізуйте методи класу для введеннявиведення даних. Також створіть методи класу для
+# виконання арифметичних операцій (додавання, віднімання, множення, ділення і т. д.). До вже
+# реалізованого класу «Дріб» додайте необхідні перевантажені методи та оператори.
+class Fraction:
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
 
-    def eat_food(self):
-        print('їсть')
+    def __str__(self):
+        return f"{self.numerator}/{self.denominator}"
+
+    def __add__(self, other):
+        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
+        new_denominator = self.denominator * other.denominator
+        return Fraction(new_numerator, new_denominator)
+
+    def __sub__(self, other):
+        new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
+        new_denominator = self.denominator * other.denominator
+        return Fraction(new_numerator, new_denominator)
+
+    def __mul__(self, other):
+        new_numerator = self.numerator * other.numerator
+        new_denominator = self.denominator * other.denominator
+        return Fraction(new_numerator, new_denominator)
+
+    def __truediv__(self, other):
+        new_numerator = self.numerator * other.denominator
+        new_denominator = self.denominator * other.numerator
+        return Fraction(new_numerator, new_denominator)
 
 
-class Dogs(Animals):
-    def __init__(self, name, age, breed):
-        self.name = name
-        self.age = age
-        self.breed = breed
-
-
-class Cats(Animals):
-    pass
-
-
-# створення екземпляру класу
-my_dog = Dogs("Rex", 2, "bulldog")
-# методи
-# my_dog.eat_food()
-# my_dog.move()
-print(my_dog.name)
-
+fraction1 = Fraction(1, 2)
+fraction2 = Fraction(3, 4)
+print(fraction1 + fraction2)
