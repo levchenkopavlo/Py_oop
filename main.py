7da3a7399f5e116254cf1fe65da9e69ab720cc2a
levchenkopavlo <levchenkopavlo@gmail.com>
2024-03-16 11:48:38 +0200

    lab2 task4 edit

--- main.py
+++ main.py
@@ -3,17 +3,28 @@
 # необхідні перевантажені методи та оператори.
 
 class Book:
-    def __init__(self, tittle, author, genre, capacity):
+    def __init__(self, tittle="", author="", genre="", year=None, capacity=0):
         self.tittle = tittle
         self.author = author
         self.genre = genre
         self.capacity = capacity
+        self.year = year
 
-          jk    def __str__(self):
-        return f"Назва: {self.tittle}\nАвтор: {self.author}\nЖанр: {self.genre}"
+    def input_data(self):
+        self.tittle = input("Введіть назву книги: ")
+        self.author = input("Введіть автора: ")
+        self.genre = input("Введіть жанр: ")
+        self.year = int(input("Введіть рік випуску: "))
+        self.capacity = int(input("Введіть кількість сторінок: "))
+
+    def __str__(self):
+        return [self.tittle, self.author, self.genre, self.year, self.capacity]
+
+    def show(self):
+        return f"Назва: {self.tittle};\nАвтор: {self.author};\nЖанр: {self.genre};\nYear: {self.year};\nСторінок: {self.capacity}."
 
     def __eq__(self, other):
-        return self.brand == other.brand and self.model == other.model and self.year == other.year
+        return self.tittle == other.tittle and self.author == other.author
 
     def __lt__(self, other):
         return self.capacity < other.capacity
@@ -21,12 +32,19 @@ class Book:
     def __gt__(self, other):
         return self.capacity > other.capacity
 
-    def __abs__(self):
-        return f"ABS function is unlocked"
+    def __le__(self, other):
+        return self.capacity <= other.capacity
 
-    def start_engine(self):
-        print(f"Двигун {self.brand} {self.model} запущено")
+    def __ge__(self, other):
+        return self.capacity >= other.capacity
+
+    def __ne__(self, other):
+        return self.tittle != other.tittle or self.author != other.author
 
 
-book1 = Book("Гарі Поттер","Джоан Роулінг", "фентезі")
-book1.display_info()
+book1 = Book("Гарі Поттер і філософський камінь", "Джоан Роулінг", "фентезі", 1997, 550)
+book2 = Book("Чужак", "Макс Фрай", "фентезі", 1996, 420)
+print(book1.show())
+print(book1.__str__())
+print(book2.__str__())
+print(book1==book2)
