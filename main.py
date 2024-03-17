adfbe48ce6ce99844ce3ecf003c98d4e0a07165f
levchenkopavlo <levchenkopavlo@gmail.com>
2024-03-17 21:35:21 +0200

    hw2 task4

--- main.py
+++ main.py
@@ -1,37 +1,54 @@
-# Завдання 1
-#  Створіть клас «Дріб». Збережіть у класі чисельник
-# та знаменник. Реалізуйте методи класу для введеннявиведення даних. Також створіть методи класу для
-# виконання арифметичних операцій (додавання, віднімання, множення, ділення і т. д.). До вже
-# реалізованого класу «Дріб» додайте необхідні перевантажені методи та оператори.
-class Fraction:
-    def __init__(self, numerator, denominator):
-        self.numerator = numerator
-        self.denominator = denominator
+# Завдання 4
+# Реалізуйте клас «Годинник». Збережіть у класі:
+# назву моделі годинника, виробника годинника, рік
+# випуску, ціну годинника, тип годинника (наручний,
+# настінний і т. д.). Реалізуйте конструктор та методи
+# класу для введення-виведення даних, а також для
+# інших операцій. Використовуйте механізм перевантаження методів.
+
+class Clock:
+    def __init__(self, brand="", model="", year=0, clockType="", value=0):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.clockType = clockType
+        self.value = value
+
+    def input_data(self):
+        while True:
+            self.brand = input("Введіть назву бренду: ")
+            self.model = input("Введіть модель: ")
+            year = input("Введіть рік випуску: ")
+            self.clockType = input("Введіть тип: ")
+            value = input("Введіть ціну: ")
+            if self.brand == "" or self.model == "" or not year.isdigit() or not value.isdigit():
+                print("Помилка вводу даних. Введіть знову.")
+                continue
+            else:
+                self.year = int(year)
+                self.value = int(value)
+                break
 
     def __str__(self):
-        return f"{self.numerator}/{self.denominator}"
+        return [self.brand, self.model, self.year, self.clockType, self.value]
 
-    def __add__(self, other):
-        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
-        new_denominator = self.denominator * other.denominator
-        return Fraction(new_numerator, new_denominator)
+    def show(self):
+        return f"Brand: {self.brand};\nModel: {self.model};\nYear: {self.year};\nType: {self.clockType};\nValue: {self.value}."
 
-    def __sub__(self, other):
-        new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
-        new_denominator = self.denominator * other.denominator
-        return Fraction(new_numerator, new_denominator)
+    def __eq__(self, other):
+        return self.brand == other.brand and self.model == other.model and self.year == other.year
 
-    def __mul__(self, other):
-        new_numerator = self.numerator * other.numerator
-        new_denominator = self.denominator * other.denominator
-        return Fraction(new_numerator, new_denominator)
+    def __lt__(self, other):
+        return self.value < other.value
 
-    def __truediv__(self, other):
-        new_numerator = self.numerator * other.denominator
-        new_denominator = self.denominator * other.numerator
-        return Fraction(new_numerator, new_denominator)
+    def __gt__(self, other):
+        return self.value > other.value
 
+    def __le__(self, other):
+        return self.value <= other.value
 
-fraction1 = Fraction(1, 2)
-fraction2 = Fraction(3, 4)
-print(fraction1 + fraction2)
+    def __ge__(self, other):
+        return self.value >= other.value
+
+    def __ne__(self, other):
+        return self.brand != other.brand or self.model != other.model
