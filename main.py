814dcee285a47b8a5b7013e59ffc14815a56a776
levchenkopavlo <levchenkopavlo@gmail.com>
2024-03-18 20:49:19 +0200

    hw2 task5

--- main.py
+++ main.py
@@ -4,3 +4,36 @@
 # та методи класу для введення-виведення даних, а також
 # для інших операцій. Використовуйте механізм переван таження методів.
 class Site:
+    def __init__(self, name="", url="", description=""):
+        self.name = name
+        self.url = url
+        self.description = description
+
+    def input_data(self):
+        while True:
+            self.name = input("Введіть назву сайту: ")
+            self.url = input("Введіть адресу: ")
+            self.description = input("Введіть короткий опис сайту: ")
+            if self.name == "" or self.url == "":
+                print("Помилка вводу даних. Введіть знову.")
+                continue
+            else:
+                break
+
+    def __str__(self):
+        return [self.name, self.url, self.description]
+
+    def show(self):
+        return f"Назва сайту: {self.name};\nАдреса: {self.url};\nОпис: {self.description}."
+
+    def __eq__(self, other):
+        return self.url == other.url
+
+    def __lt__(self, other):
+        return len(self.url) < len(other.url)
+
+    def __gt__(self, other):
+        return len(self.url) > len(other.url)
+
+    def __ne__(self, other):
+        return self.url != other.url
