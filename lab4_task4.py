c272420fec592d6c65848e4fb4f8d703f9efb2ef
levchenkopavlo <levchenkopavlo@gmail.com>
2024-03-21 20:26:22 +0200

    lab4 1-4

--- lab4_task4.py
+++ lab4_task4.py
@@ -2,3 +2,16 @@
 # Створіть клас FileUtils, який має метод класу
 # count_lines, який приймає шлях до файлу і повертає
 # кількість рядків у файлі.
+
+class FileUtils:
+    @staticmethod
+    def count_lines(path):
+        try:
+            with open(path, "r", encoding="utf-8") as fileToRead:
+                data = fileToRead.readlines()
+                return len(data)
+        except Exception as e:
+            print(e)
+
+
+print(FileUtils.count_lines("text_file1.txt"))
