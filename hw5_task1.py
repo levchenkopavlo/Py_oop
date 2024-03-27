c8d050854fc97dd49bb5a25528ed4e8a126ba304
levchenkopavlo <levchenkopavlo@gmail.com>
2024-03-27 21:53:25 +0200

    hw5 1-3 commit 1

---
+++ hw5_task1.py
@@ -0,0 +1,22 @@
+# Завдання 1
+# Створіть клас Device, який містить інформацію про пристрій.
+# За допомогою механізму успадкування реалізуйте клас
+# Coffee Machine (містить інформацію про кавомашину), клас
+# Blender (містить інформацію про блендер), клас MeatGrinder
+# (містить інформацію про м’ясорубку).
+# Кожен із класів має містити необхідні для роботи методи.
+class Device:
+    def __init__(self, brand, model, size, voltage):
+        pass
+
+
+class CoffeMachine(Device):
+    pass
+
+
+class Blender(Device):
+    pass
+
+
+class MeatGrinder(Device):
+    pass
